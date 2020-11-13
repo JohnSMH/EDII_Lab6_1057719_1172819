d2f3aa90587aea2a048e4f26a60778f407db3393
Juan Sebastián Mejía Hernández <2005003410@ceroble.edu.gt>
2020-11-12 21:28:04 -0600

    Algoritmo terminado

---
+++ Lab6/Lab1/Models/Datos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
+namespace Lab1.Models
+{
+    public class Datos
+    {
+        public string Word { get; set; }
+        public int Levels { get; set; }
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+
+        public Datos(string word, int levels, int rows, int columns)
+        {
+            Word = word;
+            Levels = levels;
+            Rows = rows;
+            Columns = columns;
+        }
+        public Datos() { }
+
+
+    }
+}
---
+++ RSA prueba/RSA.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace huffman_prueba
+{
+    class RSA
+    {
+        
+        public keypair Generatekeys(int p, int q) {
+            //por ahora asumir valores de p q y e
+            p = 61;
+            q = 53;
+            //Por añadir generar e con sus reglas
+            int e = 17;
+            
+
+            int n = p * q;
+            int phi = (p - 1) * (q - 1);
+            //int e = Erastothenes(phi);
+
+            int d = Euclides(phi, e);
+            //Llave publica n,e
+            //Llave privada n,d
+            key publica = new key(n,e);
+            key privada = new key(n,d);
+            return new keypair(publica,privada);
+        }
+
+        public int Euclides(int phi, int e) {
+            int upperright = phi;
+            int upperleft = phi;
+            int lowerleft = e;
+            int lowerright = 1;
+
+            while (lowerleft != 1) {
+                int div = upperleft / lowerleft;
+
+                int temp = lowerleft;
+
+                lowerleft = upperleft - (div * lowerleft);
+                upperleft = temp;
+
+                temp = lowerright;
+
+                lowerright = upperright - (div * lowerright);
+                upperright = temp;
+
+                if (lowerright < 0)
+                {
+                    lowerright = lowerright + phi;
+                }
+
+            }
+            return lowerright;
+        }
+
+        public byte[] CipherAndDecipher(int m,int e,int n) {
+            double c = (Math.Pow(m,e))%n;
+            return BitConverter.GetBytes(c);
+        }
+
+        
+
+        public int Erastothenes(int phi) {
+            Random randomizer = new Random();
+
+            
+
+            return 0;
+        }
+        
+    }
+}
